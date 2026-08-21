f41a1d5c66b3a5aa7a2e0552cd797983430f4714
lukastaniic <lukastannic@gmail.com>
2026-08-21 23:07:15 +0200

    Event model & factory

---
+++ app/Models/Event.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\EventFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Event extends Model
+{
+    /** @use HasFactory<EventFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'location',
+        'starts_at',
+        'ends_at',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'starts_at' => 'datetime',
+            'ends_at' => 'datetime',
+        ];
+    }
+
+    public function ticketTypes(): HasMany
+    {
+        return $this->hasMany(TicketType::class);
+    }
+}
