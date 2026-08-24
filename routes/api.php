ad11f7326ecf3b6820bb0cb7e43bb3cd7d8652c3
lukastaniic <lukastannic@gmail.com>
2026-08-24 02:36:51 +0200

    Ticket Type Controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketTypeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -12,11 +13,20 @@ Route::apiResource('events', EventController::class)->only([
     'show',
 ]);
 
+Route::get('/events/{event}/ticket-types', [TicketTypeController::class, 'index']);
+Route::get('/ticket-types/{ticketType}', [TicketTypeController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function (): void {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'me']);
 
     Route::apiResource('events', EventController::class)->only([
+        'store',
+        'update',
+        'destroy',
+    ]);
+
+    Route::apiResource('ticket-types', TicketTypeController::class)->only([
         'store',
         'update',
         'destroy',
